46557aee1bdc4ef9b946bbc107c68d5476ec8388
xelzt <bysieckisebastian@gmail.com>
2023-01-06 22:12:45 +0100

    Update Dungeon Level and Space Ship Level
    Fixed issue with too dark light on Dungeon level after loading from main menu. Added script to change color of portal shard. Added asset of mirror, wall button and wall lamps

---
+++ puzzle_solver_game/Assets/Scripts/ColorChanger.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using UnityEngine;
+
+public class ShardColorChanger : MonoBehaviour
+{
+
+    [SerializeField]
+    private GameObject shard;
+
+    private void ChangeShardColour(Color col)
+    {
+        var shard_renderer = shard.GetComponent<Renderer>();
+        if (shard_renderer != null) {
+            shard_renderer.material.SetColor("_Color", col);
+        }
+    }
+}
