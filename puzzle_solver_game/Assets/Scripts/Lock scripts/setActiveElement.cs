3735c828f502a9e19580f25a7ba191f08d1b57d5
MatthewBon <mattbonar99@gmail.com>
2023-01-10 22:11:48 +0100

    First commit
    Work in progress

--- puzzle_solver_game/Assets/Scripts/Lock scripts/setActiveElement.cs
+++ puzzle_solver_game/Assets/Scripts/Lock scripts/setActiveElement.cs
@@ -6,16 +6,34 @@ public class setActiveElement : MonoBehaviour
 {
     [SerializeField]
     public GameObject gameObj;// Object to active/hide
+    public Collider gameObjCollider;// Object to active/hide
+    public bool activeMode;
     void Start()
     {
         PlayerPrefs.DeleteAll();
+        if (gameObj.name == "Nexus")
+        {
+            gameObjCollider = GetComponent<Collider>();
+        }
     }
 
     void Update()
     {
         if (PlayerPrefs.GetInt("DidElexQuest") == 1)
         {
-            gameObj.SetActive(false);
+            gameObj.SetActive(activeMode);
+            if (gameObj.name == "Nexus")
+            {
+                gameObjCollider.enabled = true;
+            }
+        }
+
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Debug.Log("In trigger");
         }
     }
 }
