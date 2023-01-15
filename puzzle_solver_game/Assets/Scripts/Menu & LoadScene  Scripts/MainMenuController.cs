6c2eed3a7b8d98488f7bd0759e293b18e5b33fb8
MatthewBon <mattbonar99@gmail.com>
2023-01-15 02:12:19 +0100

    Disable doors to mirror's room
    Simplifi if condition in MainMenuController and remove unused variables

--- puzzle_solver_game/Assets/Scripts/Menu & LoadScene  Scripts/MainMenuController.cs
+++ puzzle_solver_game/Assets/Scripts/Menu & LoadScene  Scripts/MainMenuController.cs
@@ -5,15 +5,13 @@ using UnityEngine.UI;
 
 public class MainMenuController : MonoBehaviour
 {
-    public GameObject LoadGamePanel;
     public GameObject secondLvlButton;
     private Button btn;
-    public float transitionTime = 1f;
 
     private void Start()
     {
         btn = secondLvlButton.GetComponent<Button>();
-        btn.interactable = (PlayerPrefs.GetInt("Lvl_1_Passed") == 1 && !btn.interactable) ? true : false;
+        btn.interactable = (PlayerPrefs.GetInt("Lvl_1_Passed") == 1 && !btn.interactable);
     }
     public void Quit()
     {
