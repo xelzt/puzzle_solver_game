80537c307b71010166aea16e4bae48dc4f411814
MatthewBon <mattbonar99@gmail.com>
2023-01-31 00:23:01 +0100

    minor fix in game   elements, change lock_elex to be more user friendlly
    minor fix in game   elements, change lock_elex to be more user friendlly

--- puzzle_solver_game/Assets/Scripts/Menu & LoadScene  Scripts/MainMenuController.cs
+++ puzzle_solver_game/Assets/Scripts/Menu & LoadScene  Scripts/MainMenuController.cs
@@ -6,12 +6,18 @@ using UnityEngine.UI;
 public class MainMenuController : MonoBehaviour
 {
     public GameObject secondLvlButton;
+    public Image secondLvlImage;
+    public Sprite[] sprites;
     private Button btn;
 
     private void Start()
     {
         btn = secondLvlButton.GetComponent<Button>();
         btn.interactable = (PlayerPrefs.GetInt("Lvl_1_Passed") == 1 && !btn.interactable);
+        if (btn.interactable)
+        {
+            secondLvlImage.sprite = sprites[1];
+        } else secondLvlImage.sprite = sprites[0];
     }
     public void Quit()
     {
